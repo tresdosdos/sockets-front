ae94cd883d8037af94d8602c43fcaf8eba4a4949
Vitaliy Diachuk <vitaliystrelok8@gmail.com>
2019-09-19 08:49:55 +0300

    qq

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import React from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
-const socket = io('localhost:8080');
+const socket = io(
+    process.env.NODE_ENV === 'production'
+        ? 'https://socckets.herokuapp.com/'
+        : 'localhost:8080');
 
 socket.on('connect', () => {
-    console.log(socket.disconnected); // false
+    console.log('connected');
 });
 
 class App extends React.Component {
     state = {
-        text: ''
+        text: '',
+        messages: [],
     };
+
+    componentDidMount() {
+        socket.on('text', (msg) => {
+            const messages = [...this.state.messages];
+
+            messages.push(msg);
+
+            this.setState({
+                messages
+            });
+        });
+    }
 
     handleClick = () => {
         socket.emit('text', this.state.text)
-    }
+    };
 
     handleChange = (e) => {
         this.setState({
@@ -23,11 +39,22 @@ class App extends React.Component {
         })
     };
 
+    configureItems = () => {
+        return this.state.messages.map(msg => (<p key={msg}>{msg}</p>))
+    };
+
     render() {
+        const items = this.configureItems();
+
         return (
-            <div className="App">
-                <input value={this.state.text} onChange={this.handleChange}/>
-                <button onClick={this.handleClick}>отправить</button>
+            <div className="content-wrapper">
+                <div className="content">
+                    <input value={this.state.text} onChange={this.handleChange}/>
+                    <button onClick={this.handleClick}>отправить</button>
+                    <div>
+                        {items}
+                    </div>
+                </div>
             </div>
         );
     }
